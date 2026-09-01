3ca0e16f5df0bfee588279ca57c45580017b69a4
Matt Myers <nt3058@gmail.com>
2026-09-01 18:40:18 -0400

    Docker launcher now uses host network
    This may change later.
    
    Signed-off-by: Matt Myers <nt3058@gmail.com>

--- launch_docker.sh
+++ launch_docker.sh
@@ -2,8 +2,6 @@
 # launches the stm32-env
 # assumes stm32-env has already been built
 
-sudo docker run --rm -it \
-  --user $(id -u):$(id -g) \
-  -v $(pwd):/workspace \
-  -w /workspace \
-  stm32-env
+#Note --network host uses the host system's network space.
+#TODO consider making a docker specific network
+sudo docker run --rm -it --network host --user $(id -u):$(id -g) -v $(pwd):/workspace -w /workspace stm32-env
