10c448123aa241b1ec14a318d78f054688de71d7
Matt Myers <nt3058@gmail.com>
2026-09-01 18:41:12 -0400

    Added new debug and launch script
    debug_emu.sh launches a gdb server with the arm app
    launch_emu.sh runs the app in qemu

---
+++ source/launch_emu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Note: this script must be launched within the docker environment see README.md for details
+qemu-system-arm -M netduinoplus2 -cpu cortex-m4 -nographic -kernel ./build/qemu_app
