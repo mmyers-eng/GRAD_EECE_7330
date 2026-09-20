9054733961964a04d33ee9b8b808152d6a9f95c0
Matt Myers <nt3058@gmail.com>
2026-09-20 15:20:12 -0400

    CMake changes and cleanup
    1. Adds explicit debug flags
    2. Adds target file path (elf)
    3. Debugging with GDB and printf and exit syscalls are now fully functional
    
    Signed-off-by: Matt Myers <nt3058@gmail.com>

--- source/launch_emu.sh
+++ source/launch_emu.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Note: this script must be launched within the docker environment see README.md for details
-qemu-system-arm -M netduinoplus2 -cpu cortex-m4 -nographic -kernel ./build/qemu_app
+qemu-system-arm -M netduinoplus2 -cpu cortex-m4 -nographic -kernel ./build/qemu_app.elf
