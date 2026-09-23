7e0c31a4b89f3d8437c169df5e9726fa6dd7a937
Matt Myers <nt3058@gmail.com>
2026-09-23 07:54:45 -0400

    Moved TFML example to app
    Need better control over code for debugging
    
    Signed-off-by: Matt Myers <nt3058@gmail.com>

--- source/app/main.cpp
+++ source/app/main.cpp
@@ -1,12 +1,13 @@
 #include <cstdint>
 #include <cstdio>
+#include "tfml_test.hpp"
 #include "driver_bsp.h"
 
 extern int hello_world_test(int argc, char* argv[]);
 extern "C" int main() {
 	init_bsp();
 	printf("test of printf\n\r");
-	hello_world_test(0, 0);
+	run_tfml_test();
 	while (true) {
 		// Main loop
 	}
